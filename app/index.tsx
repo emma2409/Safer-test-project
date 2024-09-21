cc571fb0c07182a81ecb7f33d809dd5b5ddef2ec
Emmanuel <emmangombe1@gmail.com>
2024-09-21 10:35:33 +0200

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import {Text, SafeAreaView} from "react-native";
+
+export default () =>{
+    return(
+        <SafeAreaView>
+            <Text>Hello world!</Text>
+        </SafeAreaView>
+    )
+}
